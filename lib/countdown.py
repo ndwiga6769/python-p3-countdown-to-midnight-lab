8ca2d42b6f9a17f83b177a2eaf4ed7868bac410c
danwycliff <danwycliff6769@gmail.com>
2023-09-14 22:31:20 +0300

    codes

--- lib/countdown.py
+++ lib/countdown.py
@@ -1,1 +1,20 @@
 # your code goes here!
+# x = 1
+# while x < 10:
+#     print(f"{x} is less than 10")
+#     x += 1
+import time
+def countdown(number):
+    while number > 0:
+        print(f'{number} SECOND(S)!')
+        number -= 1
+    print("HAPPY NEW YEAR!")
+
+def countdown_with_sleep(number):
+    while number > 0
+        print(f'{number} SECOND(S)!')
+        time.sleep(1)
+        number -= 1
+        print("HAPPY NEW YEAR!")
+
+
